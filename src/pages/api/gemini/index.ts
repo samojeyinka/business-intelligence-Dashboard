4650cd8901df372908e17bb37bd88281708b7577
Ojeyinka Ifeoluwa Samuel <samuelojeyinka@gmail.com>
2025-04-11 17:28:01 +0100

    trying sevral ways

--- src/pages/api/gemini/index.ts
+++ src/pages/api/gemini/index.ts
@@ -3,7 +3,7 @@ import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { initialMessage } from "@/lib/data";
 
 const google = createGoogleGenerativeAI({
-  apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY || "",
+  apiKey: process.env.GOOGLE_API_KEY || "",
 });
 
 export const runtime = "edge";
